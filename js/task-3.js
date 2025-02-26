92f5f1aa6815cde399c7f14faeab91e50d9dd502
masenkq <stepamaria8@gmail.com>
2025-02-26 20:08:47 +0100

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,35 @@
+// Доповни об’єкт profile методами для роботи з його властивостями.
+
+// Метод changeUsername(newName) повинен приймати рядок (нове ім’я) в параметр newName та змінювати значення властивості username на нове. Нічого не повертає.
+// Метод updatePlayTime(hours) повинен приймати число (кількість годин) у параметр hours та збільшити на нього значення властивості playTime. Нічого не повертає.
+// Метод getInfo() має повертати рядок формату <Username> has <amount> active hours!, де <Username> — це ім’я профілю, а <amount> — кількість ігрових годин.
+
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+
+  // Метод для зміни імені профілю
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  // Метод для оновлення ігрового часу
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  // Метод для отримання інформації про профіль
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  }
+};
+
+// Перевірка методів
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
